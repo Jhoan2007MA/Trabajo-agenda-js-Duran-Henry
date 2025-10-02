ed9aa78ae3da6d16b98d35569dbc21b7a6fcd986
Jhoan2007MA <217262098+Jhoan2007MA@users.noreply.github.com>
2025-10-02 16:37:44 -0500

    feat: :sparkles: Commit 2
    Adiccion de db.json y demas.

--- App/Components/navMenu/navMenu.js
+++ App/Components/navMenu/navMenu.js
@@ -1,49 +1,73 @@
 export class NavMenu extends HTMLElement{
-    constructor(){
-        super();
-        this.render();
-    }
-    render(){
-        this.innerHTML = /* html */ `
-        <style rel="stylesheet">
-          @import "./App/Components/navMenu/menuStyle.css";
-        </style>
-          <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#" data-verocultar='["c"]'>Contactos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#" data-verocultar='["ct"]'>Citas contactos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                  </li>
-                </ul>
-              </div>
+  constructor(){
+      super();
+      this.render();
+  }
+  render(){
+      this.innerHTML = /* html */ `
+      <style rel="stylesheet">
+        @import "./App/Components/navMenu/menuStyle.css";
+      </style>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#" data-verocultar='["c"]'>Contactos</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" data-verocultar='["countries"]'>Countries</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" data-verocultar='["cities"]'>Cities</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" data-verocultar='["regions"]'>Regions</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" data-verocultar='["companies"]'>Companies</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" data-verocultar='["branches"]'>Branches</a>
+                </li>
+              </ul>
             </div>
-          </nav>        
-        `;
-        this.querySelectorAll(".nav-link").forEach((val, id) => {
-          val.addEventListener("click", (e)=>{
-              let data = JSON.parse(e.target.dataset.verocultar);
-              let mainContent = document.querySelector('#mainContent');
-              mainContent.innerHTML= "";
-              switch (data[0]){
-                case 'c':
-                  mainContent.innerHTML="<contacto-component></contacto-component>";
-                  break;
-              }
-              e.stopImmediatePropagation();
-              e.preventDefault();
-          })
-      });
-    }
+          </div>
+        </nav>        
+      `;
+      this.querySelectorAll(".nav-link").forEach((val, id) => {
+        val.addEventListener("click", (e)=>{
+            let data = JSON.parse(e.target.dataset.verocultar);
+            let mainContent = document.querySelector('#mainContent');
+            mainContent.innerHTML= "";
+            switch (data[0]){
+              case 'c':
+                mainContent.innerHTML="<contacto-component></contacto-component>";
+                break;
+              case 'countries':
+                mainContent.innerHTML="<country-component></country-component>";
+                break;
+              case 'regions':
+                mainContent.innerHTML="<region-component></region-component>";
+                break;
+              case 'cities':
+                mainContent.innerHTML="<city-component></city-component>";
+                break;
+              case 'companies':
+                mainContent.innerHTML="<company-component></company-component>";
+                break;
+              case 'branches':
+                mainContent.innerHTML="<branch-component></branch-component>";
+                break;
+            }
+            e.stopImmediatePropagation();
+            e.preventDefault();
+        })
+    });
+  }
 }
 customElements.define("nav-menu",NavMenu);
